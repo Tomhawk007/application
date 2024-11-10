585944e8e59d9396157c278b2c6322b6bd9b62bd
Tomhawk <tomhawk01012000@gmail.com>
2024-11-11 04:52:41 +0530

    complete first step

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
   return (
     <>
     <Login/>
-    {/* <Navbar title="Ushereel" profile="Profile" 
+    <Navbar title="Ushereel" profile="Profile" 
     savedPosts="Saved Posts" analytics="Analytics" 
     setting="Settings" subs="Subscription"/>
    
     <div className='container'>
       <TextForm heading="Post your Startup reels here"/>
-    </div> */}
+    </div>
     
 
     </>
